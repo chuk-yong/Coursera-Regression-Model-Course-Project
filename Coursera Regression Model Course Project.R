be3186bb1699a7ef587d965415d150e69a5fb4db
Chuk Yong <chuk_yong@hotmail.com>
2016-12-31 23:49:51 +0800

    update 2

--- Coursera Regression Model Course Project.R
+++ Coursera Regression Model Course Project.R
@@ -12,6 +12,14 @@ mtcars$carb <- factor(mtcars$carb)
 mpgAuto <- subset(mtcars, mtcars$am == "Auto", select = c(mpg))
 mpgMan <- subset(mtcars, mtcars$am == "Man", select = c(mpg))
 t.test(mpgAuto$mpg, mpgMan$mpg)
+fit <- lm(mpg ~ am, data = mtcars)
+fit2 <- lm(mpg ~ am+cyl, data = mtcars)
+fit3 <- lm(mpg ~ am+cyl+disp, data = mtcars)
+fit4 <- lm(mpg ~ am+cyl+disp+hp, data = mtcars)
+fit5 <- lm(mpg ~ am+cyl+disp+hp+wt, data = mtcars)
+
+
+summary(fit)
 pairs(mpg ~ ., data = mtcars)
 
 
